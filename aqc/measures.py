a618175743c2e092e9e497e1461ee5f514ff0f2e
Yurin N <3dsYND@gmail.com>
2021-05-05 15:44:21 +0300

    [fix] Fix use of measures when simulating with CPU

--- aqc/measures.py
+++ aqc/measures.py
@@ -8,25 +8,25 @@ def I(channel, output=None, *args, **kwargs):
   return abs(channel.run(*args, **kwargs))**2
 
 def eta(channel, *args, **kwargs):
-  return I(channel, *args, **kwargs).sum(axis=(-1, -2)).get() * channel.grid.delta**2
+  return (I(channel, *args, **kwargs).sum(axis=(-1, -2)) * channel.grid.delta**2).item()
 
 def mean_x(channel, *args, **kwargs):
   kwargs["pupil"] = False
-  return (I(channel, *args, **kwargs) * channel.grid.get_x()).sum(axis=(-1, -2)).get() * channel.grid.delta**2
+  return ((I(channel, *args, **kwargs) * channel.grid.get_x()).sum(axis=(-1, -2)) * channel.grid.delta**2).item()
 
 def mean_y(channel, *args, **kwargs):
   kwargs["pupil"] = False
-  return (I(channel, *args, **kwargs) * (-1) * channel.grid.get_y()).sum(axis=(-1, -2)).get() * channel.grid.delta**2
+  return ((I(channel, *args, **kwargs) * (-1) * channel.grid.get_y()).sum(axis=(-1, -2)) * channel.grid.delta**2).item()
 
 def mean_r(channel, *args, **kwargs):
   kwargs["pupil"] = False
   intensity = I(channel, *args, **kwargs)
-  return np.sqrt((intensity * channel.grid.get_x()).sum(axis=(-1, -2)).get()**2 + (intensity * channel.grid.get_y()).sum(axis=(-1, -2)).get()**2) * channel.grid.delta**2
+  return (np.sqrt((intensity * channel.grid.get_x()).sum(axis=(-1, -2))**2 + (intensity * channel.grid.get_y()).sum(axis=(-1, -2))**2) * channel.grid.delta**2).item()
 
 def mean_x2(channel, *args, **kwargs):
   kwargs["pupil"] = False
-  return (I(channel, *args, **kwargs) * channel.grid.get_x()**2).sum(axis=(-1, -2)).get() * channel.grid.delta**2
+  return ((I(channel, *args, **kwargs) * channel.grid.get_x()**2).sum(axis=(-1, -2)) * channel.grid.delta**2).item()
 
 def mean_r2(channel, *args, **kwargs):
   kwargs["pupil"] = False
-  return cupy.asnumpy((I(channel, *args, **kwargs) * channel.grid.get_rho2()).sum(axis=(-1, -2)) * channel.grid.delta**2)
+  return ((I(channel, *args, **kwargs) * channel.grid.get_rho2()).sum(axis=(-1, -2)) * channel.grid.delta**2).item()
