85b5242de49f0a33f61835ed9b190792cdb9bb50
Yurin N <3dsYND@gmail.com>
2021-05-07 00:53:32 +0300

    [feat] Add simple GUI for parallel simulation

--- aqc/simulations/simulations.py
+++ aqc/simulations/simulations.py
@@ -1,22 +1,55 @@
+from IPython import display
+import ipywidgets as widgets
+from jupyter_ui_poll import ui_events
+
+
 class Simulations:
     def __init__(self, channel, simulations: list):
         self.channel = channel
         self.simulations = simulations
 
+    def iter(self):
+        for propagation_step, (propagation_result, phase_screen) in enumerate(self.channel.generator(pupil=False, store_output=True)):
+            for simulation in self.simulations:
+                if simulation.type == "phase_screen":
+                    simulation.process(phase_screen, propagation_step)
+                    simulation.iteration += 1
+                elif simulation.type == "propagation":
+                    simulation.process(propagation_result, propagation_step)
+        for simulation in self.simulations:
+            if simulation.type == "output":
+                simulation.process(self.channel.output)
+            if simulation.type == "pupil":
+                simulation.process(self.channel.pupil.output(self.channel.output))
+
+            if simulation.type != "phase_screen":
+                simulation.iteration += 1
+
     def run(self):
         while True:
-            for propagation_step, (propagation_result, phase_screen) in enumerate(self.channel.generator(pupil=False, store_output=True)):
-                for simulation in self.simulations:
-                    if simulation.type == "phase_screen":
-                        simulation.process(phase_screen, propagation_step)
-                        simulation.iteration += 1
-                    elif simulation.type == "propagation":
-                        simulation.process(propagation_result, propagation_step)
-            for simulation in self.simulations:
-                if simulation.type == "output":
-                    simulation.process(self.channel.output)
-                if simulation.type == "pupil":
-                    simulation.process(self.channel.pupil.output(self.channel.output))
-                
-                if simulation.type != "phase_screen":
-                    simulation.iteration += 1
+            self.iter()
+
+
+class SimulationsGUI(Simulations):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.simulations_dropdown = widgets.Dropdown(
+            options=[(type(simulation).__name__, i)for i, simulation in enumerate(self.simulations)],
+            value=0,
+            description='Simulation:',
+            )
+
+    def update_display(self):
+        display.display(self.simulations_dropdown)
+        active_simulation = self.simulations_dropdown.value
+        if self.simulations[active_simulation].iteration > 0:
+            self.simulations[active_simulation].print()
+        display.clear_output(wait=True)
+
+    def run(self):
+        self.update_display()
+        with ui_events() as poll:
+            while True:
+                self.iter()
+                poll(1)
+                self.update_display()
